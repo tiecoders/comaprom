f581b64be50575dee339bfdf04c49bb7a8a0e2d2
ahmedhannachi-cg <ahmed.hannachi@capgemini.com>
2019-03-06 17:12:03 +0100

    remove js form submit

--- js/scripts.js
+++ js/scripts.js
@@ -209,34 +209,6 @@ $(window).on("load",function (){
 
     });
 
-
-    // contact form validator
-    // $('#contact-form').validator();
-
-    $('#contact-form').on('submit', function (e) {
-        e.preventDefault();
-            var url = "contact.php";
-            $.ajax({
-                type: "POST",
-                url: url,
-                data: $(this).serialize(),
-                success: function (data)
-                {  
-                    alert(data);
-                    var messageAlert = 'alert-' + data.type;
-                    var messageText = data.message;
-
-                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-                    if (messageAlert && messageText) {
-                        $('#contact-form').find('.messages').html(alertBox);
-                        $('#contact-form')[0].reset();
-                    }
-                }
-            });
-            //return false;
-        
-    });
-
 });
 
 
